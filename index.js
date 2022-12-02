fc57f9ea1bc91dedd7c69b4ab914c511b3e1c29e
tmdwid2 <115774345+tmdwid2@users.noreply.github.com>
2022-12-03 00:39:58 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({extended: true})) 
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true})) 
+var db;
+
+const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs');
+
+MongoClient.connect("mongodb+srv://tmdwid2:dlwlsdl52!@cluster0.ggygzzw.mongodb.net/?retryWrites=true&w=majority", function(err, client){
+  if (err) return console.log(err)
+     db = client.db('moonDB');
+
+    console.log('DB connected')
+
+  app.listen(8080, function() {
+    console.log('listening on 8080')
+  })
+})
+
+
+app.get('/', function(req, res) { 
+  res.sendFile(__dirname +'/index.html')
+  })
+
+app.get('/write', function(req, res) { 
+    res.sendFile(__dirname +'/write.html')
+  })
+
+  app.get('/login', function(req, res) { 
+    res.sendFile(__dirname +'/login.html')
+  })
+
+app.get('/list', function(req, res) {
+  db.collection('login').find().toArray(function(err, result){
+    console.log(result);
+    res.render('list.ejs', {loginfo : result})
+  })
+})
+
+  app.post('/add', function(req, res){
+    db.collection('config').findOne({name : 'totalcount'}, function(err, result){
+      var mycount = result.count;
+      db.collection('login').insertOne( { _id : (mycount + 1), email : req.body.email, password : req.body.password } , function(){
+        db.collection('config').updateOne({name:'totalcount'}, { $inc: {count:1} }, function(err, result) {
+          if(err) return console.log(err)
+          console.log('save complete')
+          res.send('send complete....');
+        });
+      });
+    });
+    
+  });
